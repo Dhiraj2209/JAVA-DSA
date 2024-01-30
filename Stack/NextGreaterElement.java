3994bd65efa4029b02bd9e97410d8bebb47f2e64
Dhiraj Prajapati <dhiruuuuu22@gmail.com>
2024-01-30 11:47:54 +0530

    Resolved issues in Stack.

---
+++ Stack/NextGreaterElement.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class NextGreaterElement {
+    
+    public static void Next_Greater_Element(int arr[]){
+        Stack<Integer> s = new Stack<>();
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
--- Stack/StackUsingJCF.java
+++ Stack/StackUsingJCF.java
@@ -1,4 +1,4 @@
-package Stack;
+// package Stack;
 import java.util.Stack;
 
 public class StackUsingJCF{
