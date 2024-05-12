87fb69a7e5bbcbd81a557b9d56b332bedf4c40be
Dhiraj Prajapati <118041529+Dhiraj2209@users.noreply.github.com>
2024-05-12 22:47:52 +0530

    Optimized Majority_Element_in_Array

--- Array & ArrayList/Majority_Element_in_Array.java
+++ Array & ArrayList/Majority_Element_in_Array.java
@@ -1,26 +1,50 @@
 public class Majority_Element_in_Array {
     
-    public static int majorityelement(int arr[]){
+    public static int findMajorityElement(int[] arr) {
+        int candidate = findCandidate(arr);
+        if (isMajority(arr, candidate)) {
+            return candidate;
+        } else {
+            return -1;
+        }
+    }
+    
+    private static int findCandidate(int[] arr) {
+        int candidate = arr[0];
+        int count = 1;
         
-        for(int i = 0; i < arr.length ;i++){
-            int value = arr[i];
-            int count = 0;
-            for(int j = 0 ; j < arr.length ;j++){
-                if(arr[j] == value){
-                    count++;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == candidate) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    candidate = arr[i];
+                    count = 1;
                 }
             }
-            if(count > (arr.length)/2){
-                return arr[i];
+        }
+        
+        return candidate;
+    }
+    
+    private static boolean isMajority(int[] arr, int candidate) {
+        int count = 0;
+        for (int num : arr) {
+            if (num == candidate) {
+                count++;
             }
         }
-        return -1;
+        return count > arr.length / 2;
     }
-    public static void main(String args[]){
-        int arr[] = {1, 1, 2, 3, 5, 10 };
-        if((majorityelement(arr)) == -1){
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 3, 5, 10};
+        int result = findMajorityElement(arr);
+        if (result != -1) {
+            System.out.println("Majority Element: " + result);
+        } else {
             System.out.println("No Majority Element present in Array.");
-        }else
-        System.out.println("Majority Element : " + majorityelement(arr));
+        }
     }
 }
